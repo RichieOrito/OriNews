347e5582f491b7155a2a179e152afa88aa1a8b07
RichieOrito <richard.omondi@student.moringaschool.com>
2022-01-30 23:04:21 +0300

    This commit creates dynamic routes

--- app/main/views.py
+++ app/main/views.py
@@ -5,6 +5,13 @@ from . import main
 @main.route('/')
 def index():
 
-      '''
+    '''
     View root page function that returns the index page and its data
     '''
+
+@main.route('/articles/<source_id>')
+def source(source_id):
+    '''
+    View articles page function that returns articles from the specified sources.
+    '''
+    
