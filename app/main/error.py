5be9c8f925abb0b50bedd3a3b679cc2de880bd36
RichieOrito <richard.omondi@student.moringaschool.com>
2022-01-31 02:29:24 +0300

    This commit updates the error

--- app/main/error.py
+++ app/main/error.py
@@ -3,11 +3,11 @@ from flask import render_template
 from . import main
 
 @main.app_errorhandler(404)
-def error_handler(error):
+def four_Ow_four(error):
     '''
     Function to render the 404 error page
     '''
     title = 'Not Found | Orinews'
-    return render_template('error.html', error_msg = error, title=title),404
+    return render_template('fourOwfour.html', error_msg = error, title=title),404
 
 
