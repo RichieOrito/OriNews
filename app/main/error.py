c4d31b42df40ade8c489c8037a283ab5d40ca361
RichieOrito <richard.omondi@student.moringaschool.com>
2022-01-31 02:20:51 +0300

    This commit creates a function to render the 404 error page

---
+++ app/main/error.py
@@ -0,0 +1,13 @@
+from turtle import title
+from flask import render_template
+from . import main
+
+@main.app_errorhandler(404)
+def error_handler(error):
+    '''
+    Function to render the 404 error page
+    '''
+    title = 'Not Found | Orinews'
+    return render_template('error.html', error_msg = error, title=title),404
+
+
