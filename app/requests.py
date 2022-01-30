9ab847c3b9eb0f6bb298743925607c71f1c7ec01
RichieOrito <richard.omondi@student.moringaschool.com>
2022-01-31 00:24:33 +0300

    This commit creates three variables api key and base_url and set them to None we then create a function configure_request that takes in the application instance and replaces the values of the None Variables to application configuration objects

--- app/requests.py
+++ app/requests.py
@@ -2,5 +2,23 @@ import urllib.request, json
 from .models import Article, Source
 from dateutil import parser
 
+# Getting api key
+api_key = None
 
+# Getting the news base url
+base_url = None
 
+# Getting the src base url
+src_aticles_base_url = None
+
+# Getting the search base url
+search_base_url = None
+
+def configure_src_request(app):
+    global api_key, base_url, src_aticles_base_url, search_base_url
+    api_key = app.config['NEWS_API_KEY']
+    base_url = app.config['NEWS_SRC_API_BASE_URL']
+    src_aticles_base_url = app.config['ARTICLES_FROM_SRC_BASE_URL']
+    search_base_url = app.config['SEARCH_BASE_URL']
+
+    
