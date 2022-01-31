4483ad481dac7151f1527580d852f77faa419b63
RichieOrito <richard.omondi@student.moringaschool.com>
2022-01-31 03:03:31 +0300

    This commit imports the configuration settings directly to the application using the from object() method

--- app/__init__.py
+++ app/__init__.py
@@ -7,6 +7,6 @@ material = Material
 def create_app(config_name):
     app = Flask(__name__)
 
-    # setting config
-    from .requests import configure_src_request
-    configure_src_request(app)
+    #Create the app configurations.
+    app.config.from_object(config_options[config_name])
+
