4c52659a5e0547f7de2e244892e592a103d7b938
RichieOrito <richard.omondi@student.moringaschool.com>
2022-01-31 17:59:06 +0300

    I used the @manager.commnad decorator to create a new command.And created the test function that loads all the test files and runs them all

--- config.py
+++ config.py
@@ -11,8 +11,10 @@ class Config:
 
     SEARCH_BASE_URL = 'https://newsapi.org/v2/everything?q={}&apiKey={}'
 
-    pass
 
+    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+
+    SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 class ProdConfig(Config):
