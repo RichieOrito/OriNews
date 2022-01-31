372571175a3b7a8effb715e810c11b3348b92224
RichieOrito <richard.omondi@student.moringaschool.com>
2022-01-31 03:28:09 +0300

    This commit  calls the create_app function and pass in the configuration_options key so as to create the application instance.I instantiate the Manager class by passing in the app instance

--- manage.py
+++ manage.py
@@ -1,3 +1,9 @@
+from flask import app
 from app import create_app
 from flask_script import Manager, Server
 
+# Creating app instance.
+app = create_app('development')
+
+manager = Manager(app)
+manager.add_command('server', Server)
