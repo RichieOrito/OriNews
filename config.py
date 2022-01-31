8bfa3bd74cadbfe43d3f59ba8fa3bab34bd1259e
RichieOrito <richard.omondi@student.moringaschool.com>
2022-01-31 19:00:39 +0300

    This commit clears spaces

--- config.py
+++ config.py
@@ -6,14 +6,9 @@ class Config:
     '''
 
     NEWS_SRC_API_BASE_URL = 'https://newsapi.org/v2/sources?apiKey={}&language=en'
-
     ARTICLES_FROM_SRC_BASE_URL = 'https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'
-
     SEARCH_BASE_URL = 'https://newsapi.org/v2/everything?q={}&apiKey={}'
-
-
     NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
-
     SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
